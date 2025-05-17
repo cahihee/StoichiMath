cc2e5c012bb02c27b61e9222b1bd225aa7e3e989
cahihee <macha.cahia@gmail.com>
2025-05-17 20:25:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ if option == "Perhitungan Massa dan mol":
         st.write(f"Jumlah mol = {mol}")
         
         # Penyelesaian dan rumus
-            st.markdown("### 🧮 Penyelesaian")
+            st.markdown("🧮 Penyelesaian")
             st.latex(r"n = \frac{massa}{massa\ molar}")
             st.latex(f"n = \\frac{{{massa}~gram}}{{{massa_molar}~g/mol}}")
             st.latex(f"n = {mol:.4f}~mol")
