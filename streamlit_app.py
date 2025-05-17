118da4bdcc84024ef52a60674b5c8243951a8875
cahihee <macha.cahia@gmail.com>
2025-05-17 22:20:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def load_lottieurl(url):
     return r.json()
 
 # URL animasi Lottie
-lottie_url = "https://assets2.lottiefiles.com/packages/lf20_x62chJ.json"
+lottie_url = "https://assets10.lottiefiles.com/packages/lf20_jtbfg2nb.json"
 lottie_json = load_lottieurl(lottie_url)
 
 # Tampilan halaman About
